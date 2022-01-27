37780a2286ec4aa677621ce66d786b7f425cd876
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-27 20:42:50 +0530

    add app.get

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("Server is up and running.");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
