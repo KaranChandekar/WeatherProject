0bef4dc561de2872484fa4157c99ff6e73e17568
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-28 18:32:58 +0530

    add url

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Nagpur&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
   res.send("Server is up and running.");
 });
 
