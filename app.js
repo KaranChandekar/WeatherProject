975c987e1c9d02d5855b250684807d1263e97096
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-28 18:52:56 +0530

    add response.on

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const { STATUS_CODES } = require("http");
 const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Nagpur&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Nagpur&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
 
   https.get(url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+      console.log(data);
+    });
   });
-  
+
   res.send("Server is up and running.");
 });
 
