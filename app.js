8b6be140e027f98bc2e60585e1566a54da263c3f
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-29 18:37:39 +0530

    change place

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get("/", function (req, res) {
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Nagpur&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
 
   https.get(url, function (response) {
     console.log(response.statusCode);
@@ -14,7 +14,7 @@ app.get("/", function (req, res) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
-      res.send("<h1>The temperature in Nagpur is " + temp + " degree Celcius.</h1>");
+      res.send("<h1>The temperature in London is " + temp + " degree Celcius.</h1>");
     });
   });
 });
