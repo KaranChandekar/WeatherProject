5d60e6b512ed272df6c40480f9c4b806ddd7dea9
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-29 21:24:54 +0530

    add query

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
+  
+  const query = "London";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=59c81e0f9698c05cf5739452f34babb9&units=metric";
 
   https.get(url, function (response) {
     console.log(response.statusCode);
