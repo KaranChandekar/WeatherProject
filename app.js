0c2992379741549b86bf971a7680a0e25cbedb90
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-29 22:00:47 +0530

    use bodyParser

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
