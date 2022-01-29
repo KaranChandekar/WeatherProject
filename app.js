887da47b5b7f3a50f30e2bb9faa917af567f051c
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-29 22:21:20 +0530

    modify .write

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post("/", function(req, res) {
     const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
     res.write("<p>The weather is currently " + weatherDescription + "<p>");
-    res.write("<h1>The temperature in London is " + temp + "&#176" + " Celcius.</h1>");
+    res.write("<h1>The temperature in " + query + " is " + temp + "&#176" + " Celcius.</h1>");
     res.write(`<img src="` + imgUrl + `">`);
 
     res.send();
