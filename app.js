c87844b0a8107e49e8ce8faf4bde6d0777adb793
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-29 22:18:55 +0530

    post cityName

--- app.js
+++ app.js
@@ -12,31 +12,30 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function(req, res) {
-  console.log("Post request recieved!");
-})
+  
+  const query = req.body.cityName;
+  const apiKey = "59c81e0f9698c05cf5739452f34babb9";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
-// const query = "London";
-// const apiKey = "59c81e0f9698c05cf5739452f34babb9";
-// const unit = "metric";
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+  https.get(url, function (response) {
+  console.log(response.statusCode);
 
-// https.get(url, function (response) {
-//   console.log(response.statusCode);
+  response.on("data", function (data) {
+    const weatherData = JSON.parse(data);
+    const temp = weatherData.main.temp;
+    const weatherDescription = weatherData.weather[0].description;
+    const icon = weatherData.weather[0].icon;
+    const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-//   response.on("data", function (data) {
-//     const weatherData = JSON.parse(data);
-//     const temp = weatherData.main.temp;
-//     const weatherDescription = weatherData.weather[0].description;
-//     const icon = weatherData.weather[0].icon;
-//     const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    res.write("<p>The weather is currently " + weatherDescription + "<p>");
+    res.write("<h1>The temperature in London is " + temp + "&#176" + " Celcius.</h1>");
+    res.write(`<img src="` + imgUrl + `">`);
 
-//     res.write("<p>The weather is currently " + weatherDescription + "<p>");
-//     res.write("<h1>The temperature in London is " + temp + "&#176" + " Celcius.</h1>");
-//     res.write(`<img src="` + imgUrl + `">`);
-
-//     res.send();
-//   });
-// });
+    res.send();
+  });
+});
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
