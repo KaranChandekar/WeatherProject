4bc9e3331411a553e2ac22b7e57ce944a48e2f97
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-29 19:16:56 +0530

    add icon in url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", function (req, res) {
       const temp = weatherData.main.temp;
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      const imgUrl = "http://openweathermap.org/img/wn/10d@2x.png";
+      const imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
       res.write("<p>The weather is currently " + weatherDescription + "<p>");
       res.write("<h1>The temperature in London is " + temp + " degree Celcius.</h1>");
